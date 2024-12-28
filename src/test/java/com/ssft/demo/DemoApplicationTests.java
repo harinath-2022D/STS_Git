faf36d64ffde5fdc88c205a582fc20db6dfd2ac1
Harinath <harireddyakkati.91@gmail.com>
2024-12-28 21:49:46 +0530

    main branch

---
+++ src/test/java/com/ssft/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
